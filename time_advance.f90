43500872f53e66b50fe552885866e62410601767
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2018-05-04 09:37:52 +0100

    Added ydriftknob factor in parallel nonlinearity term as it originates from bhat x curvature . grad B. Needed if testing a symmetry of the equations.

--- time_advance.f90
+++ time_advance.f90
@@ -430,7 +430,8 @@ contains
     par_nl_fac = 0.5*rhostar*spread(spec%stm*spec%zt,1,nztot)*spread(gradpar(1,:),2,nspec)
 
     if (.not. allocated(par_nl_curv)) allocate (par_nl_curv(-nzgrid:nzgrid,nspec))
-    par_nl_curv = -rhostar*geo_surf%rgeo*geo_surf%betaprim*drhodpsi &
+    ! ydriftknob is here because this term comes from bhat x curvature . grad B
+    par_nl_curv = -ydriftknob*rhostar*geo_surf%rgeo*geo_surf%betaprim*drhodpsi &
          *spread(dbdzed(1,:)*gradpar(1,:)/bmag(1,:),2,nspec)/spread(spec%zt,1,nztot)
 
     if (.not. allocated(par_nl_drifty)) allocate (par_nl_drifty(naky,-nzgrid:nzgrid))
