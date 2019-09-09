aaa92263522a340669cea72f95808b94e225195e
Michael Barnes <michael.barnes@physics.ox.ac.uk>
2019-09-09 13:36:15 +0100

    Added option in gyro_averages to gyro-average alpha-dependent quantities in prep for full_flux_surface version.

--- gyro_averages.f90
+++ gyro_averages.f90
@@ -1,6 +1,6 @@
 module gyro_averages
 
-  public :: aj0x, aj0v, aj1v
+  public :: aj0x, aj0v, aj1v, aj0a
   public :: init_bessel, finish_bessel
   public :: gyro_average
   public :: gyro_average_j1
@@ -37,11 +37,12 @@ contains
     use zgrid, only: nzgrid, nztot
     use vpamu_grids, only: vperp2, nmu, nvpa
     use kt_grids, only: naky, nakx, nalpha
+    use kt_grids, only: naky_all, ikx_max
+    use kt_grids, only: swap_kxky
     use stella_layouts, only: kxkyz_lo, vmu_lo
     use stella_layouts, only: iky_idx, ikx_idx, iz_idx, is_idx, imu_idx
     use spfunc, only: j0, j1
     use stella_transforms, only: transform_alpha2kalpha
-!    use stella_transforms, only: transform_kalpha2alpha
 
     implicit none
 
@@ -53,6 +54,7 @@ contains
 
     real, dimension (:), allocatable :: aj0_alpha
     complex, dimension (:), allocatable :: aj0_kalpha
+    real, dimension (:,:,:), allocatable :: kperp2_swap
 
     if (bessinit) return
     bessinit = .true.
@@ -60,10 +62,6 @@ contains
     if (.not.allocated(aj0v)) then
        allocate (aj0v(nmu,kxkyz_lo%llim_proc:kxkyz_lo%ulim_alloc))
        aj0v = 0.
-    end if
-    if (.not.allocated(aj0x)) then
-       allocate (aj0x(naky,nakx,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
-       aj0x = 0.
     end if
     if (.not.allocated(aj1v)) then
        allocate (aj1v(nmu,kxkyz_lo%llim_proc:kxkyz_lo%ulim_alloc))
@@ -87,9 +85,10 @@ contains
     if (full_flux_surface) then
        allocate (aj0_alpha(nalpha))
        allocate (aj0_kalpha(naky))
-       if (.not.allocated(ia_max_aj0a)) allocate(ia_max_aj0a(naky,nakx,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
+       allocate (kperp2_swap(naky_all,ikx_max,nalpha))
+       if (.not.allocated(ia_max_aj0a)) allocate(ia_max_aj0a(naky_all,ikx_max,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
        if (.not.allocated(aj0a)) then
-          allocate(aj0a(naky,naky,nakx,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
+          allocate(aj0a(naky,naky_all,ikx_max,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
           aj0a = 0.
        end if
           
@@ -98,12 +97,14 @@ contains
           is = is_idx(vmu_lo,ivmu)
           imu = imu_idx(vmu_lo,ivmu)
           do iz = -nzgrid, nzgrid
-             do ikx = 1, nakx
-                do iky = 1, naky
+             do ia = 1, nalpha
+                call swap_kxky (kperp2(:,:,ia,iz), kperp2_swap(:,:,ia))
+             end do
+             do ikx = 1, ikx_max
+                do iky = 1, naky_all
                    do ia = 1, nalpha
-                      arg = spec(is)%smz*sqrt(vperp2(ia,iz,imu)*kperp2(iky,ikx,ia,iz))/bmag(ia,iz)
-                      aj0x(iky,ikx,iz,ivmu) = j0(arg)
-                      aj0_alpha(ia) = aj0x(iky,ikx,iz,ivmu)
+                      arg = spec(is)%smz*sqrt(vperp2(ia,iz,imu)*kperp2_swap(iky,ikx,ia))/bmag(ia,iz)
+                      aj0_alpha(ia) = j0(arg)
                    end do
                    ! fourier transform aj0_alpha
                    ! note that fourier coefficients aj0_kalpha have
@@ -127,7 +128,13 @@ contains
        end if
 
        deallocate (aj0_kalpha, aj0_alpha)
+       deallocate (kperp2_swap)
     else
+       if (.not.allocated(aj0x)) then
+          allocate (aj0x(naky,nakx,-nzgrid:nzgrid,vmu_lo%llim_proc:vmu_lo%ulim_alloc))
+          aj0x = 0.
+       end if
+
        ia = 1
        do ivmu = vmu_lo%llim_proc, vmu_lo%ulim_proc
           is = is_idx(vmu_lo,ivmu)
@@ -193,6 +200,7 @@ contains
     implicit none
 
     if (allocated(aj0v)) deallocate (aj0v)
+    if (allocated(aj1v)) deallocate (aj1v)
     if (allocated(aj0x)) deallocate (aj0x)
     if (allocated(aj0a)) deallocate (aj0a)
     if (allocated(ia_max_aj0a)) deallocate (ia_max_aj0a)
@@ -203,13 +211,57 @@ contains
 
   subroutine gyro_average_kxky_local (field, iz, ivmu, gyro_field)
 
+    use physics_flags, only: full_flux_surface
+    use kt_grids, only: naky, nakx
+    use kt_grids, only: ikx_max
+    use kt_grids, only: swap_kxky_ordered, swap_kxky_back_ordered
+
     implicit none
 
     complex, dimension (:,:), intent (in) :: field
     integer, intent (in) :: iz, ivmu
     complex, dimension (:,:), intent (out) :: gyro_field
 
-    gyro_field = aj0x(:,:,iz,ivmu)*field
+    integer :: ia, iky, ikx
+    integer :: idx
+    integer :: naky_all
+    complex, dimension (:,:), allocatable :: field_kyall, gyro_field_kyall
+
+    if (full_flux_surface) then
+       naky_all = 2*naky-1
+       ! need to switch from ky>=0 and all kx
+       ! to kx>=0 and all ky (using reality condition)
+       allocate (field_kyall(naky_all,ikx_max))
+       allocate (gyro_field_kyall(naky_all,ikx_max)) ; gyro_field_kyall = 0.
+       call swap_kxky_ordered (field, field_kyall)
+       ! NB: J0(kx,ky) = J0(-kx,-ky)
+       do ikx = 1, ikx_max
+          do iky = 1, naky_all
+             ! account for contributions from less positive ky values (and zero)
+             do ia = 1, min(ia_max_aj0a(iky,ikx,iz,ivmu),iky)
+                idx = iky-ia+1
+                gyro_field_kyall(iky,ikx) = gyro_field_kyall(iky,ikx) &
+                     + aj0a(ia,idx,ikx,iz,ivmu)*field_kyall(idx,ikx)
+             end do
+             ! account for contributions from more positive ky values
+             if (ia_max_aj0a(iky,ikx,iz,ivmu) > 1 .and. iky /= naky_all) then
+                do ia = 2, min(ia_max_aj0a(iky,ikx,iz,ivmu),naky_all-iky+1)
+                   idx = iky+ia-1
+                   gyro_field_kyall(iky,ikx) = gyro_field_kyall(iky,ikx) &
+                        + aj0a(ia,idx,ikx,iz,ivmu)*field_kyall(idx,ikx)
+                end do
+             end if
+          end do
+       end do
+       call swap_kxky_back_ordered (gyro_field_kyall, gyro_field)
+       deallocate (field_kyall, gyro_field_kyall)
+    else
+       gyro_field = aj0x(:,:,iz,ivmu)*field
+       ! not sure if below necessary, but wanting to avoid
+       ! problems of allocatable array not being allocated
+       allocate (field_kyall(1,1)) ; deallocate (field_kyall)
+       allocate (gyro_field_kyall(1,1)) ; deallocate (gyro_field_kyall)
+    end if
 
   end subroutine gyro_average_kxky_local
 
@@ -223,7 +275,14 @@ contains
     integer, intent (in) :: ivmu
     complex, dimension (:,:,-nzgrid:,:), intent (out) :: gyro_field
 
-    gyro_field = spread(aj0x(:,:,:,ivmu),4,ntubes)*field
+    integer :: iz, it
+
+    do it = 1, ntubes
+       do iz = -nzgrid, nzgrid
+          call gyro_average (field(:,:,iz,it), iz, ivmu, gyro_field(:,:,iz,it))
+       end do
+    end do
+!    gyro_field = spread(aj0x(:,:,:,ivmu),4,ntubes)*field
 
   end subroutine gyro_average_kxkyz_local
 
