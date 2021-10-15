f2ea02fc018a39daae875282bbf1ca1a7f8ff647
Peter Hill <peter.hill@york.ac.uk>
2021-10-15 14:40:41 +0100

    Initial pass of fprettify

--- volume_averages.f90
+++ volume_averages.f90
@@ -1,158 +1,157 @@
 module volume_averages
 
-  public :: init_volume_averages, finish_volume_averages
-  public :: fieldline_average
-  public :: volume_average
+   public :: init_volume_averages, finish_volume_averages
+   public :: fieldline_average
+   public :: volume_average
 ! public :: flux_surface_average
-  
-  public :: mode_fac
-  
-  private
 
-  interface fieldline_average
-     module procedure fieldline_average_real
-     module procedure fieldline_average_complex
-  end interface
+   public :: mode_fac
 
-  real, dimension (:), allocatable :: mode_fac
+   private
+
+   interface fieldline_average
+      module procedure fieldline_average_real
+      module procedure fieldline_average_complex
+   end interface
+
+   real, dimension(:), allocatable :: mode_fac
 
 contains
 
-  subroutine init_volume_averages
+   subroutine init_volume_averages
 
-    use zgrid, only: nzgrid, nztot, ntubes, delzed
-    use kt_grids, only: nalpha, aky, nakx, naky, rho_d_clamped
-    use stella_geometry, only: geo_surf, drhodpsi
-    use stella_geometry, only: geo_surf, jacob, djacdrho, q_as_x, dVolume
-    use physics_flags, only: full_flux_surface, radial_variation
+      use zgrid, only: nzgrid, nztot, ntubes, delzed
+      use kt_grids, only: nalpha, aky, nakx, naky, rho_d_clamped
+      use stella_geometry, only: geo_surf, drhodpsi
+      use stella_geometry, only: geo_surf, jacob, djacdrho, q_as_x, dVolume
+      use physics_flags, only: full_flux_surface, radial_variation
 
-    implicit none
+      implicit none
 
-    real :: dqdrho
+      real :: dqdrho
 
-    if (.not.allocated(mode_fac)) then
-       allocate (mode_fac(naky)) ; mode_fac = 2.0
-       if (aky(1)<epsilon(0.)) mode_fac(1) = 1.0
-    end if
+      if (.not. allocated(mode_fac)) then
+         allocate (mode_fac(naky)); mode_fac = 2.0
+         if (aky(1) < epsilon(0.)) mode_fac(1) = 1.0
+      end if
 
-    dqdrho = geo_surf%shat * geo_surf%qinp/ geo_surf%rhoc
-    if (.not.allocated(dVolume)) allocate (dVolume(nalpha,nakx,-nzgrid:nzgrid))
+      dqdrho = geo_surf%shat * geo_surf%qinp / geo_surf%rhoc
+      if (.not. allocated(dVolume)) allocate (dVolume(nalpha, nakx, -nzgrid:nzgrid))
 
-    !dVolume contains the volume element jacob, which may vary with x or alpha
-    ! NB: dVolume does not contain the factor dx, as this should always be uniform
-    dVolume = spread(jacob*spread(delzed,1,nalpha),2,nakx)
-    if (q_as_x) then
-      dVolume = dVolume / (dqdrho*drhodpsi)
-    endif
-
-    if (radial_variation) then
+      !dVolume contains the volume element jacob, which may vary with x or alpha
+      ! NB: dVolume does not contain the factor dx, as this should always be uniform
+      dVolume = spread(jacob * spread(delzed, 1, nalpha), 2, nakx)
       if (q_as_x) then
-        dVolume = dVolume*(1. + spread(spread(rho_d_clamped,1,nalpha),3,nztot) & 
-                  *(spread(djacdrho/jacob,2,nakx) - geo_surf%d2qdr2/dqdrho     &
-                    + geo_surf%d2psidr2*drhodpsi))
-      else
-        dVolume = dVolume*(1. + spread(spread(rho_d_clamped,1,nalpha),3,nztot) & 
-                              * spread(djacdrho/jacob,2,nakx))
-      endif
-    endif
+         dVolume = dVolume / (dqdrho * drhodpsi)
+      end if
 
-    if (full_flux_surface) then
-      !something should go here
-    endif
+      if (radial_variation) then
+         if (q_as_x) then
+            dVolume = dVolume * (1.+spread(spread(rho_d_clamped, 1, nalpha), 3, nztot) &
+                                 * (spread(djacdrho / jacob, 2, nakx) - geo_surf%d2qdr2 / dqdrho &
+                                    + geo_surf%d2psidr2 * drhodpsi))
+         else
+            dVolume = dVolume * (1.+spread(spread(rho_d_clamped, 1, nalpha), 3, nztot) &
+                                 * spread(djacdrho / jacob, 2, nakx))
+         end if
+      end if
 
-    !avoid the double counting at the zed boundaries
-    dVolume(:,:,-nzgrid)= 0.5*dVolume(:,:,-nzgrid) 
-    dVolume(:,:, nzgrid)= 0.5*dVolume(:,:, nzgrid) 
+      if (full_flux_surface) then
+         !something should go here
+      end if
 
-  end subroutine init_volume_averages
+      !avoid the double counting at the zed boundaries
+      dVolume(:, :, -nzgrid) = 0.5 * dVolume(:, :, -nzgrid)
+      dVolume(:, :, nzgrid) = 0.5 * dVolume(:, :, nzgrid)
 
-  subroutine finish_volume_averages
-    use stella_geometry, only: dVolume
-    implicit none
-    if (allocated(mode_fac)) deallocate (mode_fac)
-    if (allocated(dVolume)) deallocate (dVolume)
-  end subroutine finish_volume_averages
+   end subroutine init_volume_averages
 
+   subroutine finish_volume_averages
+      use stella_geometry, only: dVolume
+      implicit none
+      if (allocated(mode_fac)) deallocate (mode_fac)
+      if (allocated(dVolume)) deallocate (dVolume)
+   end subroutine finish_volume_averages
 
-  !==============================================
-  !============ FIELD LINE AVERAGE ==============
-  !==============================================
-  subroutine fieldline_average_real (unavg, avg)
+   !==============================================
+   !============ FIELD LINE AVERAGE ==============
+   !==============================================
+   subroutine fieldline_average_real(unavg, avg)
 
-    use zgrid, only: nzgrid, ntubes
-    use kt_grids, only: nakx, naky
-    use stella_geometry, only: dl_over_b
+      use zgrid, only: nzgrid, ntubes
+      use kt_grids, only: nakx, naky
+      use stella_geometry, only: dl_over_b
 
-    implicit none
+      implicit none
 
-    real, dimension (:,:,-nzgrid:,:), intent (in) :: unavg
-    real, dimension (:,:), intent (out) :: avg
+      real, dimension(:, :, -nzgrid:, :), intent(in) :: unavg
+      real, dimension(:, :), intent(out) :: avg
 
-    integer :: it, ia
+      integer :: it, ia
 
-    ia = 1
+      ia = 1
 
-    avg = 0.0
-    do it = 1, ntubes
-       avg = avg + sum(spread(spread(dl_over_b(ia,:),1,naky),2,nakx)*unavg(:,:,:,it),dim=3)
-    end do
-    avg = avg/real(ntubes)
-    
-  end subroutine fieldline_average_real
+      avg = 0.0
+      do it = 1, ntubes
+         avg = avg + sum(spread(spread(dl_over_b(ia, :), 1, naky), 2, nakx) * unavg(:, :, :, it), dim=3)
+      end do
+      avg = avg / real(ntubes)
 
-  subroutine fieldline_average_complex (unavg, avg)
+   end subroutine fieldline_average_real
 
-    use zgrid, only: nzgrid, ntubes
-    use kt_grids, only: nakx, naky
-    use stella_geometry, only: dl_over_b
+   subroutine fieldline_average_complex(unavg, avg)
 
-    implicit none
+      use zgrid, only: nzgrid, ntubes
+      use kt_grids, only: nakx, naky
+      use stella_geometry, only: dl_over_b
 
-    complex, dimension (:,:,-nzgrid:,:), intent (in) :: unavg
-    complex, dimension (:,:), intent (out) :: avg
+      implicit none
 
-    integer :: it, ia
+      complex, dimension(:, :, -nzgrid:, :), intent(in) :: unavg
+      complex, dimension(:, :), intent(out) :: avg
 
-    ia = 1
+      integer :: it, ia
 
-    avg = 0.0
-    do it = 1, ntubes
-       avg = avg + sum(spread(spread(dl_over_b(ia,:),1,naky),2,nakx)*unavg(:,:,:,it),dim=3)
-    end do
-    avg = avg/real(ntubes)
+      ia = 1
 
-  end subroutine fieldline_average_complex
+      avg = 0.0
+      do it = 1, ntubes
+         avg = avg + sum(spread(spread(dl_over_b(ia, :), 1, naky), 2, nakx) * unavg(:, :, :, it), dim=3)
+      end do
+      avg = avg / real(ntubes)
 
-  !==============================================
-  !============== VOLUME AVERAGE ================
-  !==============================================
-  subroutine volume_average (unavg, avg)
+   end subroutine fieldline_average_complex
 
-    use zgrid, only: nzgrid, ntubes
-    use kt_grids, only: naky, nakx
-    use stella_geometry, only: dl_over_b
+   !==============================================
+   !============== VOLUME AVERAGE ================
+   !==============================================
+   subroutine volume_average(unavg, avg)
 
-    implicit none
+      use zgrid, only: nzgrid, ntubes
+      use kt_grids, only: naky, nakx
+      use stella_geometry, only: dl_over_b
 
-    complex, dimension (:,:,-nzgrid:,:), intent (in) :: unavg
-    real, intent (out) :: avg
+      implicit none
 
-    integer :: iky, ikx, iz, it, ia
+      complex, dimension(:, :, -nzgrid:, :), intent(in) :: unavg
+      real, intent(out) :: avg
 
-    ia = 1
+      integer :: iky, ikx, iz, it, ia
 
-    avg = 0.
-    do it = 1, ntubes
-       do iz = -nzgrid, nzgrid
-          do ikx = 1, nakx
-             do iky = 1, naky
-                avg = avg + real(unavg(iky,ikx,iz,it)*conjg(unavg(iky,ikx,iz,it)))*mode_fac(iky)*dl_over_b(ia,iz)
-             end do
-          end do
-       end do
-    end do
-    avg = avg/real(ntubes)
+      ia = 1
 
-  end subroutine volume_average
+      avg = 0.
+      do it = 1, ntubes
+         do iz = -nzgrid, nzgrid
+            do ikx = 1, nakx
+               do iky = 1, naky
+                  avg = avg + real(unavg(iky, ikx, iz, it) * conjg(unavg(iky, ikx, iz, it))) * mode_fac(iky) * dl_over_b(ia, iz)
+               end do
+            end do
+         end do
+      end do
+      avg = avg / real(ntubes)
+
+   end subroutine volume_average
 
 end module volume_averages
