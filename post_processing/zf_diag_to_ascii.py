fixed a wrong index in zf_diag_to_ascii.py

post_processing/zf_diag_to_ascii.py
@@ -2,9 +2,7 @@ from scipy.io import netcdf
 import numpy as np
 
 fpref='center'
-basedir = '/Users/denisst-onge/stella/build/multi_test/'
-fpref='master'
-basedir = '/Users/denisst-onge/stella/build/RH/'
+basedir = '/work/e607/e607/dstonge/zf_test/'
 center_file = basedir +fpref + '.out.nc'
 
 
@@ -24,7 +22,7 @@ zf_sum = np.zeros(zf_shape)
 zf_sum[0,:,:] = zf_diag[0,:,:]
 
 for i in range(nt):
-  for j in range(1,zf_shape[2]):
+  for j in range(zf_shape[2]):
       zf_sum[i,:,j] = zf_sum[i-1,:,j]+zf_diag[i,:,j]*(t[i]-t[i-1])
 
 #what measurements are in zf_diag, along with the time
@@ -43,17 +41,23 @@ header = ['time',
           ]
 
 #print the header
-print('#'),
+#print('#')
+print('#',end=' ')
 for i in range(len(header)):
-    print('['+str(i+1) + ']'),
-    print(header[i]),
+#    print('['+str(i+1) + ']'),
+#    print(header[i]),
+    print('['+str(i+1) + ']',end=' ')
+    print(header[i],end=' ')
 print('')
 
 #print the data
 for i in range(nt):
-  print(t[i]),
+# print(t[i]),
+  print(t[i],end=' ')
   for j in range(ncol):
-    print(zf_diag[i,ind,j]+zf_diag[i,nx-ind,j]),
-  for j in range(1,ncol):
-    print(zf_sum[i,ind,j]+zf_sum[i,nx-ind,j]),
+#   print(zf_diag[i,ind,j]+zf_diag[i,nx-ind,j]),
+    print(zf_diag[i,ind,j]+zf_diag[i,nx-ind,j],end=' ')
+  for j in range(ncol):
+#   print(zf_sum[i,ind,j]+zf_sum[i,nx-ind,j]),
+    print(zf_sum[i,ind,j]+zf_sum[i,nx-ind,j],end=' ')
   print('')