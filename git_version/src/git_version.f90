dc35638cbbb6b468eca215ebbd7fcb053a5cfaeb
ZedThree <ZedThree@users.noreply.github.com>
2021-12-14 17:19:08 +0000

    [skip ci] Apply fprettify changes

--- git_version/src/git_version.f90
+++ git_version/src/git_version.f90
@@ -4,31 +4,31 @@
 !> into the binary. The implementations are in a submodule to avoid
 !> recompilation cascades.
 module git_version
-  implicit none
-  interface
-    !> Returns the git version from `git describe`
-    !>
-    !> This looks like: `{tag}-g{hash}[-dirty]`
-    module function get_git_version()
-      character(:), allocatable :: get_git_version
-    end function get_git_version
+   implicit none
+   interface
+      !> Returns the git version from `git describe`
+      !>
+      !> This looks like: `{tag}-g{hash}[-dirty]`
+      module function get_git_version()
+         character(:), allocatable :: get_git_version
+      end function get_git_version
 
-    !> Returns the git hash of the current commit
-    module function get_git_hash(length_in)
-      integer, optional, intent(in) :: length_in
-      integer :: length
-      character(:), allocatable :: get_git_hash
-    end function get_git_hash
+      !> Returns the git hash of the current commit
+      module function get_git_hash(length_in)
+         integer, optional, intent(in) :: length_in
+         integer :: length
+         character(:), allocatable :: get_git_hash
+      end function get_git_hash
 
-    !> Return "-dirty" if the repository has modifications to tracked
-    !> files, or the empty string otherwise
-    module function get_git_state()
-      character(:), allocatable :: get_git_state
-    end function get_git_state
+      !> Return "-dirty" if the repository has modifications to tracked
+      !> files, or the empty string otherwise
+      module function get_git_state()
+         character(:), allocatable :: get_git_state
+      end function get_git_state
 
-    !> Returns the short date (YYYY-MM-DD) of the current commit
-    module function get_git_date()
-      character(len=10) :: get_git_date
-    end function get_git_date
-  end interface
+      !> Returns the short date (YYYY-MM-DD) of the current commit
+      module function get_git_date()
+         character(len=10) :: get_git_date
+      end function get_git_date
+   end interface
 end module git_version
