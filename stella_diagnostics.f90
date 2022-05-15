dc46df9c61bbb073c8b78ad8455e588fde26ea68
rd1042 <rd1042@fusion213.its.york.ac.uk>
2022-05-15 22:19:28 +0100

    Minor bug fix in reading diagnostic-related parameters

--- stella_diagnostics.f90
+++ stella_diagnostics.f90
@@ -155,7 +155,7 @@ contains
 
       namelist /stella_diagnostics_knobs/ nwrite, navg, nsave, &
          save_for_restart, write_phi_vs_time, write_apar_vs_time, &
-         write_bpar_vs_time, write_gvmus, write_gzvs write_omega, &
+         write_bpar_vs_time, write_gvmus, write_gzvs, write_omega, &
          write_kspectra, write_moments, write_radial_fluxes, &
          write_radial_moments, write_fluxes_kxkyz, flux_norm, nc_mult
 
