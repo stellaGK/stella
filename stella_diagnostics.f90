d5ffbf7bcb26abb056a11a844a01c9d9ef0d159e
Peter Hill <peter.hill@york.ac.uk>
2022-05-19 11:53:50 +0100

    Write (kx, ky) spectra/fluxes using neasyf

--- stella_diagnostics.f90
+++ stella_diagnostics.f90
@@ -122,9 +122,7 @@ contains
       ntg_out = nzed / 2 + (nperiod - 1) * nzed
 
       !> Initiate the netcdf file with extension '.out.nc'
-      call init_stella_io(restart, write_kspectra, &
-                          write_gvmus, write_gzvs, write_moments, &
-                          write_fluxes_kxkyz)
+      call init_stella_io(restart, write_gvmus, write_gzvs, write_moments)
 
       !> Open the '.out', '.fluxes' and '.omega' file
       if (proc0) call open_loop_ascii_files(restart)
