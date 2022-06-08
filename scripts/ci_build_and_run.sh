17189b33ef21e76e798576d3e1ac06f88b317130
David Dickinson <d.dickinson@york.ac.uk>
2022-06-08 13:52:00 +0100

    First attempt at adding a debug CI build

--- scripts/ci_build_and_run.sh
+++ scripts/ci_build_and_run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 export GK_SYSTEM=gnu_ubuntu
-make -I Makefiles -j2
+make -I Makefiles -j2 $@
 
-make -I Makefiles build-pfunit-library
-make -I Makefiles check
+make -I Makefiles build-pfunit-library $@
+make -I Makefiles check $@
