202f6023984b3a934f90a2bfca0e197f51daed0c
HanneThienpondt <76125875+HanneThienpondt@users.noreply.github.com>
2022-09-07 11:39:31 +0200

    Update list_commands.py

--- stellapy/utils/commandprompt/list_commands.py
+++ stellapy/utils/commandprompt/list_commands.py
@@ -18,26 +18,30 @@ from stellapy.utils.commandprompt.bash import Bash
 #                          LIST ALL THE STELLAPY COMMANDS                      #
 #===============================================================================
 
-def list_commands(width=90):
+def list_commands(print_only=None):
+    
+    # Width of the text
+    width = 90
     
     # Start 
-    print("\n"+"".center(width,"="))
-    print("\033[1m"+"STELLAPY".center(width," ")+"\033[0m")
-    print("".center(width,"="))   
-    print("""
-    In order to use the stellapy scripts and functions one can call the functions 
-    directly from the python3 interactive prompt, or one can use the bash commands 
-    defined in stellapy/source.sh directly from the command prompt. A list of possible
-    stellapy functions that work as bash commands is shown through the command:
-        >> stellapy
-    
-    These commands can be used in the same way as bash commands, a list of options 
-    is shown for each commands by performing:
-        >> command -h
-    
-    Developed by Hanne Thienpondt. 
-    01/09/2022
-    """)
+    if print_only==None:
+        print("\n"+"".center(width,"="))
+        print("\033[1m"+"STELLAPY".center(width," ")+"\033[0m")
+        print("".center(width,"="))   
+        print("""
+        In order to use the stellapy scripts and functions one can call the functions 
+        directly from the python3 interactive prompt, or one can use the bash commands 
+        defined in stellapy/source.sh directly from the command prompt. A list of possible
+        stellapy functions that work as bash commands is shown through the command:
+            >> stellapy
+
+        These commands can be used in the same way as bash commands, a list of options 
+        is shown for each commands by performing:
+            >> command -h
+
+        Developed by Hanne Thienpondt. 
+        07/09/2022
+        """)
     
     # Start 
     print("\n"+"".center(width,"="))
@@ -45,67 +49,71 @@ def list_commands(width=90):
     print("".center(width,"="), "\n") 
     print("  Overview of the stellapy commands which work like Bash commands. ")
     print("  Developed by Hanne Thienpondt. ")
-    print("  01/09/2022")
+    print("  07/09/2022")
     print()
     print()
     
     # GUI (Graphical User Interface) 
-    print("\033[1m GUI (Graphical User Interface) \033[0m")
-    print("\033[1m ----------------------------- \033[0m", "\n")
-    print("    ", " >> stellaplotter")
-    print("    ", " >> stellaplotter_linear")
-    print("    ", " >> stellaplotter_nonlinear")
-    print()
+    if print_only==None or print_only=="GUI":
+        print("\033[1m GUI (Graphical User Interface) \033[0m")
+        print("\033[1m ----------------------------- \033[0m", "\n")
+        print("    ", " >> stellaplotter")
+        print("    ", " >> stellaplotter_linear")
+        print("    ", " >> stellaplotter_nonlinear")
+        print()
     
     # Memory management
-    print("\033[1m MEMORY MANAGEMENT \033[0m")
-    print("\033[1m ----------------- \033[0m", "\n")
-    print("    ", " >> reduce_sizeNetcdf")  
-    print("    ", " >> replace_netcdfFile")  
-    print() 
+    if print_only==None or print_only=="memory":
+        print("\033[1m MEMORY MANAGEMENT \033[0m")
+        print("\033[1m ----------------- \033[0m", "\n")
+        print("    ", " >> reduce_sizeNetcdf")  
+        print("    ", " >> replace_netcdfFile")  
+        print() 
     
     # Data processing
-    print("\033[1m DATA PROCESSING \033[0m")
-    print("\033[1m --------------- \033[0m", "\n")
-    print("    ", " >> write_dataFiles") 
-    print("          ", " >> write_dataFiles -s ini") 
-    print("          ", " >> write_dataFiles -s pot3D -t 1") 
-    print("          ", " >> write_dataFiles -s phases --skip 10")
-    print() 
+    if print_only==None or print_only=="write":
+        print("\033[1m DATA PROCESSING \033[0m")
+        print("\033[1m --------------- \033[0m", "\n")
+        print("    ", " >> write_dataFiles") 
+        print("          ", " >> write_dataFiles -s ini") 
+        print("          ", " >> write_dataFiles -s pot3D -t 1") 
+        print("          ", " >> write_dataFiles -s phases --skip 10")
+        print() 
    
     # Plot linear simulations
-    print("\033[1m PLOT LINEAR SIMULATIONS \033[0m")
-    print("\033[1m ---------------------- \033[0m", "\n")
-    print("   ", "\033[1m TIME EVOLUTION \033[0m")
-    print("   ", "\033[1m -------------- \033[0m") 
-    print("      ", " >> plot_dphiz_vs_time")  
-    print("      ", " >> plot_gamma_vs_time")   
-    print("      ", " >> plot_omega_vs_time")     
-    print() 
-    print("   ", "\033[1m SPECTRA \033[0m")
-    print("   ", "\033[1m ------- \033[0m")
-    print("      ", " >> plot_gamma_vs_ky")   
-    print("      ", " >> plot_omega_vs_ky") 
-    print("      ", " >> plot_gamma_vs_kx")   
-    print("      ", " >> plot_omega_vs_kx")   
-    print() 
-    print("   ", "\033[1m COMPLEX ANALYSIS \033[0m")
-    print("   ", "\033[1m ---------------- \033[0m")
-    print("      ", " >> plot_gamma")   
-    print("      ", " >> plot_spectra")  
-    print("      ", " >> plot_spectrum")  
-    print() 
+    if print_only==None or print_only=="linear":
+        print("\033[1m PLOT LINEAR SIMULATIONS \033[0m")
+        print("\033[1m ---------------------- \033[0m", "\n")
+        print("   ", "\033[1m TIME EVOLUTION \033[0m")
+        print("   ", "\033[1m -------------- \033[0m") 
+        print("      ", " >> plot_dphiz_vs_time")  
+        print("      ", " >> plot_gamma_vs_time")   
+        print("      ", " >> plot_omega_vs_time")     
+        print() 
+        print("   ", "\033[1m SPECTRA \033[0m")
+        print("   ", "\033[1m ------- \033[0m")
+        print("      ", " >> plot_gamma_vs_ky")   
+        print("      ", " >> plot_omega_vs_ky") 
+        print("      ", " >> plot_gamma_vs_kx")   
+        print("      ", " >> plot_omega_vs_kx")   
+        print() 
+        print("   ", "\033[1m COMPLEX ANALYSIS \033[0m")
+        print("   ", "\033[1m ---------------- \033[0m")
+        print("      ", " >> plot_spectrum     >> plot_gamma ")   
+        print("      ", " >> plot_spectra      >> plot_parameter_influence")     
+        print() 
     
     # Plot nonlinear simulations
-    print("\033[1m PLOT NONLINEAR SIMULATIONS \033[0m")
-    print("\033[1m -------------------------- \033[0m", "\n")
-    print("   ", "\033[1m TIME EVOLUTION \033[0m")
-    print("   ", "\033[1m -------------- \033[0m")  
-    print("      ", " >> plot_flux_vs_time --qflux")  
-    print("      ", " >> plot_qflux_vs_time")  
-    print("      ", " >> plot_pflux_vs_time")     
-    print("      ", " >> plot_vflux_vs_time")       
-    print() 
+    if print_only==None or print_only=="nonlinear":
+        print("\033[1m PLOT NONLINEAR SIMULATIONS \033[0m")
+        print("\033[1m -------------------------- \033[0m", "\n")
+        print("   ", "\033[1m TIME EVOLUTION \033[0m")
+        print("   ", "\033[1m -------------- \033[0m")  
+        print("      ", " >> plot_flux_vs_time --qflux")  
+        print("      ", " >> plot_qflux_vs_time")  
+        print("      ", " >> plot_pflux_vs_time")     
+        print("      ", " >> plot_vflux_vs_time")       
+        print() 
     
     print()
     return
@@ -115,8 +123,19 @@ def list_commands(width=90):
 #===============================================================================
  
 if __name__ == "__main__":
-    bash = Bash(list_commands, __doc__)  
+    
+    # Create a bash interface
+    bash = Bash(list_commands, __doc__) 
+    
+    # Data toggles to print specific parts of the commands
+    bash.add_toggle('print_only', 'linear', 'Show only the linear plotting commands.')  
+    bash.add_toggle('print_only', 'nonlinear', 'Show only the nonlinear plotting commands.')  
+    bash.add_toggle('print_only', 'GUI', 'Show only the GUI commands.')   
+    bash.add_toggle('print_only', 'memory', 'Show only the memory commands.')  
+    bash.add_toggle('print_only', 'data', 'Show only the data writing commands.')  
+    
+    # Get the arguments and launch the script
     args = bash.get_arguments()
     del args['folder']
-    list_commands(**args)   
+    list_commands(**args)    
     
