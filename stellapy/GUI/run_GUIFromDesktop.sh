26f4d66a7193d355638e18f85e37e4a0dc80e8ae
Hanne.Thienpondt <Hanne.Thienpondt@outlook.com>
2023-03-09 11:11:02 +0100

    Removed the GUI for now, it will be fixed in the future.

--- stellapy/GUI/run_GUIFromDesktop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Make sure $STELLAPY is loaded 
-. ./source.sh
-
-# Run the GUI by executing the python script, print crash logs to stella_GUI.log 
-rm $STELLAPY/GUI/stella_GUI.log
-/usr/bin/python3 $STELLAPY/GUI/stella_GUI.py >> $STELLAPY/GUI/stella_GUI.log 2>&1
