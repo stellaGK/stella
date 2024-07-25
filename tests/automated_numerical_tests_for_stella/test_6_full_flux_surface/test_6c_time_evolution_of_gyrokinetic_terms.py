a7d44c134a645efea11c176dd00b809763e215dd
GeorgiaActon <georgia.a@hotmail.co.uk>
2024-07-25 19:40:44 +0100

    Add comment to FFS test

--- tests/automated_numerical_tests_for_stella/test_6_full_flux_surface/test_6c_time_evolution_of_gyrokinetic_terms.py
+++ tests/automated_numerical_tests_for_stella/test_6_full_flux_surface/test_6c_time_evolution_of_gyrokinetic_terms.py
@@ -3,21 +3,24 @@
 #               Check all the terms in the gyrokinetic equation                #
 ################################################################################
 # These test are designed to test the terms in the gyrokinetic equation one 
-# by one. As a sanity check we start by turning of all terms to check that
-# the potential is not being evolved in time. To ensure this we need:
+# by one for the case of Full Flux Surface with modified adiabatic electrons 
+# (Boltzmann electrons). As a sanity check we start by turning of all terms to 
+# check that the potential is not being evolved in time. Then we turn the terms 
+# on one by one linearly, then all together linearly, and finally we test them 
+# when every term is turned on nonlinearly. The flags we toggle are:
 #       &physics_flags 
-#         nonlinear = .false.
-#         include_parallel_streaming = .false.
-#         include_mirror = .false.
+#         nonlinear = .false. / .true.
+#         include_parallel_streaming = .false. / .true.
+#         include_mirror = .false. / .true.
 #       /
 #       &time_advance_knobs
-#         xdriftknob = 0
-#         ydriftknob = 0
-#         wstarknob = 0 
+#         xdriftknob = 0.0 / 1.0
+#         ydriftknob = 0.0 / 1.0
+#         wstarknob = 0.0 / 1.0
 #       /
-#       &dissipation
-#         hyper_dissipation = .false.
-#       /   
+#       &knobs
+#         stream_implicit = .false. / .true.
+#         mirror_implicit = .false. / .true.
 ################################################################################
 
 # Python modules
@@ -77,7 +80,8 @@ def test_each_gyrokinetic_term_for_modified_adiabatic_full_flux_surface(tmp_path
     # Compare phi2(t) in the netCDF files
     local_netcdf_file = tmp_path / (input_filename_stem + '_parallel_streaming_implicit.out.nc') 
     expected_netcdf_file = get_stella_expected_run_directory() / f'EXPECTED_OUTPUT.{input_filename.replace(".in","")}.out.nc'   
-    compare_local_potential_with_expected_potential(local_netcdf_file, expected_netcdf_file, error=False)     
+    compare_local_potential_with_expected_potential(local_netcdf_file, expected_netcdf_file, error=False)
+
     print('  -->  The parallel streaming term for adiabatic FFS, implemented implicitly, is evolving correctly.') 
 
 #-------------------------------------------------------------------------------
@@ -116,7 +120,7 @@ def test_each_gyrokinetic_term_for_modified_adiabatic_full_flux_surface(tmp_path
     # Compare phi2(t) in the netCDF files
     local_netcdf_file = tmp_path / (input_filename_stem + '_mirror_explicit.out.nc') 
     expected_netcdf_file = get_stella_expected_run_directory() / f'EXPECTED_OUTPUT.{input_filename.replace(".in","")}.out.nc'    
-    compare_local_potential_with_expected_potential(local_netcdf_file, expected_netcdf_file, error=False)    
+    compare_local_potential_with_expected_potential(local_netcdf_file, expected_netcdf_file, error=False)  
     print('  -->  The mirror term for adiabatic FFS, implemented explicitly, is evolving correctly.')
 
 #-------------------------------------------------------------------------------
