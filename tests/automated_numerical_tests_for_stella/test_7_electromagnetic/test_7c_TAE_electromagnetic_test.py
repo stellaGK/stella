d209ef7456add11500e7793374392ce240b26fb4
GeorgiaActon <georgia.a@hotmail.co.uk>
2024-09-04 15:06:54 +0100

    Add TAE EM test
    Add TAE automatic numerical test for Electromagnetic stella

--- tests/automated_numerical_tests_for_stella/test_7_electromagnetic/test_7c_TAE_electromagnetic_test.py
+++ tests/automated_numerical_tests_for_stella/test_7_electromagnetic/test_7c_TAE_electromagnetic_test.py
@@ -1,9 +1,9 @@
 
 ################################################################################
-#               Check all the terms in the gyrokinetic equation                #
+#               Check the TAE physics for Electromagentic stella               #
 ################################################################################
-# These test are designed to test that the code correctly capture the KBM (Kinetic
-# Ballooning Mode) physics in Electromagnetic stella.
+# These test are designed to test that the code correctly capture the TAE
+# (Toroidal Alfven Eigenmode) physics in Electromagnetic stella.
 # This input has been benchmarked against GS2. The resolution has been greatly
 # reduced for the efficiency of automatic testing, but the physics is still
 # captured.
@@ -25,24 +25,24 @@ input_filename_stem = 'EM'
 stella_local_run_directory = 'Not/Run/Yet' 
 
 #-------------------------------------------------------------------------------
-#           Check whether the potential data for the KBM mode matches          #
+#           Check whether the potential data for the TAE mode matches          #
 #-------------------------------------------------------------------------------
-def test_KBM_physics_for_Electromagnetic_stella(tmp_path):
+def test_TAE_physics_for_Electromagnetic_stella(tmp_path):
     
     # Save the temporary folder <tmp_path> as a global variable so the
     # other tests can access the output files from the local stella run.
     global stella_local_run_directory
     stella_local_run_directory = tmp_path
     
-    input_filename = input_filename_stem + '_kbm.in'
+    input_filename = input_filename_stem + '_TAE.in'
     # Run stella inside of <tmp_path> based on <input_filename>
     run_local_stella_simulation(input_filename, tmp_path)
      
     # File names  
-    local_netcdf_file = tmp_path / (input_filename_stem + '_kbm.out.nc') 
+    local_netcdf_file = tmp_path / (input_filename_stem + '_TAE.out.nc') 
     expected_netcdf_file = get_stella_expected_run_directory() / f'EXPECTED_OUTPUT.{input_filename.replace(".in","")}.out.nc'   
     compare_local_potential_with_expected_potential_em(local_netcdf_file, expected_netcdf_file, error=False)
             
-    print(f'  --> The KBM (Kinetic Ballooning Mode) physics is not captured correctly.')
+    print(f'  --> The TAE (Kinetic Ballooning Mode) physics is not captured correctly.')
 
     return
