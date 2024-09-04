1814e8461bda60d9b228619bffafc70acf12b178
GeorgiaActon <georgia.a@hotmail.co.uk>
2024-09-04 14:39:08 +0100

    Add in automated numerical check for EM
    Test that KBM physics is correctly captured

---
+++ tests/automated_numerical_tests_for_stella/test_7_electromagnetic/test_7b_kbm_electromagnetic_test.py
@@ -0,0 +1,48 @@
+
+################################################################################
+#               Check all the terms in the gyrokinetic equation                #
+################################################################################
+# These test are designed to test that the code correctly capture the KBM (Kinetic
+# Ballooning Mode) physics in Electromagnetic stella.
+# This input has been benchmarked against GS2. The resolution has been greatly
+# reduced for the efficiency of automatic testing, but the physics is still
+# captured.
+################################################################################
+
+# Python modules
+import os, sys
+import pathlib 
+import numpy as np
+import xarray as xr  
+
+# Package to run stella 
+module_path = str(pathlib.Path(__file__).parent.parent.parent / 'run_local_stella_simulation.py')
+with open(module_path, 'r') as file: exec(file.read())
+
+# Global variables
+
+input_filename_stem = 'EM'  
+stella_local_run_directory = 'Not/Run/Yet' 
+
+#-------------------------------------------------------------------------------
+#           Check whether the potential data for the KBM mode matches          #
+#-------------------------------------------------------------------------------
+def test_KBM_physics_for_Electromagnetic_stella(tmp_path):
+    
+    # Save the temporary folder <tmp_path> as a global variable so the
+    # other tests can access the output files from the local stella run.
+    global stella_local_run_directory
+    stella_local_run_directory = tmp_path
+    
+    input_filename = input_filename_stem + '_kbm.in'
+    # Run stella inside of <tmp_path> based on <input_filename>
+    run_local_stella_simulation(input_filename, tmp_path)
+     
+    # File names  
+    local_netcdf_file = tmp_path / (input_filename_stem + '_kbm.out.nc') 
+    expected_netcdf_file = get_stella_expected_run_directory() / f'EXPECTED_OUTPUT.{input_filename.replace(".in","")}.out.nc'   
+    compare_local_potential_with_expected_potential_em(local_netcdf_file, expected_netcdf_file, error=False)
+            
+    print(f'  --> The KBM (Kinetic Ballooning Mode) physics is not captured correctly.')
+
+    return
