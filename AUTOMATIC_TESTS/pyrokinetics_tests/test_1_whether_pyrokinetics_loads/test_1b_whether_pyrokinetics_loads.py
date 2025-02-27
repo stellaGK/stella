977ae300321327584f854b662a5ec7a0615290bf
HanneThienpondt <76125875+HanneThienpondt@users.noreply.github.com>
2025-02-27 10:42:32 +0100

    Auto-check whether pyrokinetics can load a stella output. (#197)
    * Automatically tests if stella is compatible with the pyrokinetics module.

---
+++ AUTOMATIC_TESTS/pyrokinetics_tests/test_1_whether_pyrokinetics_loads/test_1b_whether_pyrokinetics_loads.py
@@ -0,0 +1,40 @@
+################################################################################
+#              Check whether pyrokinetics can plot stella output               #
+################################################################################
+
+# Python modules
+import os, sys
+import pathlib 
+import numpy as np
+import xarray as xr  
+
+# Package to run stella 
+module_path = str(pathlib.Path(__file__).parent.parent.parent / 'run_local_stella_simulation.py')
+with open(module_path, 'r') as file: exec(file.read())
+
+# Pyrokinetics package
+from pyrokinetics import Pyro
+
+# Global variables  
+input_filename = 'miller_linear_CBC.in'  
+local_stella_run_directory = 'Not/Run/Yet'
+
+#-------------------------------------------------------------------------------
+#                         Run local stella simulation                          #
+#-------------------------------------------------------------------------------
+def test_whether_we_can_load_pyrokinetics(tmp_path):
+    '''Run a local stella simulation in a temporary folder <tmp_path>.'''  
+    
+    # Save the temporary folder <tmp_path> as a global variable so the
+    # other tests can access the output files from the local stella run.
+    global local_stella_run_directory
+    local_stella_run_directory = tmp_path
+    
+    # Run stella inside of <tmp_path> based on <input_filename>
+    run_local_stella_simulation(input_filename, tmp_path) 
+    
+    # Load the pyro data
+    local_input_file = local_stella_run_directory / input_filename
+    pyro = Pyro(gk_file=local_input_file, gk_code="STELLA")
+    print('\n  -->  Successfully loaded pyrokinetics.')
+    return
