25d23439b1cbd3d1f5c3e0e2b131fc71d43c151c
HanneThienpondt <76125875+HanneThienpondt@users.noreply.github.com>
2025-08-18 15:52:16 +0200

    Run automatic tests on older stella releases (v0.5, v0.6 and v0.7) for now only for test 1. (#214)
    * Run automatic tests on all stella releases (only test 1 has been implemented for now).

--- AUTOMATIC_TESTS/numerical_tests/test_3_gyrokinetic_equation/test_3d_kxky_grid_options.py
+++ AUTOMATIC_TESTS/numerical_tests/test_3_gyrokinetic_equation/test_3d_kxky_grid_options.py
@@ -6,22 +6,30 @@
 ################################################################################
 
 # Python modules
+import pytest
 import os, sys
-import pathlib 
+import pathlib
 import numpy as np
-import xarray as xr  
+import xarray as xr
 
 # Package to run stella 
 module_path = str(pathlib.Path(__file__).parent.parent.parent / 'run_local_stella_simulation.py')
 with open(module_path, 'r') as file: exec(file.read())
 
 #-------------------------------------------------------------------------------
+#                           Get the stella version                             #
+#-------------------------------------------------------------------------------
+@pytest.fixture(scope="session")
+def stella_version(pytestconfig):
+    return pytestconfig.getoption("stella_version")
+
+#-------------------------------------------------------------------------------
 #                                BOX NONLINEAR                                 #
 #-------------------------------------------------------------------------------
-def test_kxky_grid_box_nonlinear(tmp_path, error=False):
+def test_kxky_grid_box_nonlinear(tmp_path, stella_version, error=False):
 
     # Run stella inside of <tmp_path>
-    run_data = run_local_stella_simulation('kxky_grid_box_nonlinear.in', tmp_path)
+    run_data = run_local_stella_simulation('kxky_grid_box_nonlinear.in', tmp_path, stella_version)
      
     # Compare phi2(t) in the netCDF files   
     compare_local_potential_with_expected_potential(run_data=run_data, error=False)    
@@ -31,10 +39,10 @@ def test_kxky_grid_box_nonlinear(tmp_path, error=False):
 #-------------------------------------------------------------------------------
 #                                 BOX LINEAR                                   #
 #-------------------------------------------------------------------------------
-def test_kxky_grid_box_linear(tmp_path, error=False):
+def test_kxky_grid_box_linear(tmp_path, stella_version, error=False):
 
     # Run stella inside of <tmp_path>
-    run_data = run_local_stella_simulation('kxky_grid_box_linear.in', tmp_path)
+    run_data = run_local_stella_simulation('kxky_grid_box_linear.in', tmp_path, stella_version)
      
     # Compare phi2(t) in the netCDF files   
     compare_local_potential_with_expected_potential(run_data=run_data, error=False)    
@@ -44,10 +52,10 @@ def test_kxky_grid_box_linear(tmp_path, error=False):
 #-------------------------------------------------------------------------------
 #                                RANGE LINEAR                                  #
 #-------------------------------------------------------------------------------
-def test_kxky_grid_range_linear(tmp_path, error=False):
+def test_kxky_grid_range_linear(tmp_path, stella_version, error=False):
 
     # Run stella inside of <tmp_path>
-    run_data = run_local_stella_simulation('kxky_grid_range_linear.in', tmp_path)
+    run_data = run_local_stella_simulation('kxky_grid_range_linear.in', tmp_path, stella_version)
      
     # Compare phi2(t) in the netCDF files   
     compare_local_potential_with_expected_potential(run_data=run_data, error=False)    
@@ -57,10 +65,10 @@ def test_kxky_grid_range_linear(tmp_path, error=False):
 #-------------------------------------------------------------------------------
 #                                RANGE LINEAR                                  #
 #-------------------------------------------------------------------------------
-def test_kxky_grid_singlemode_linear(tmp_path, error=False):
+def test_kxky_grid_singlemode_linear(tmp_path, stella_version, error=False):
 
     # Run stella inside of <tmp_path>
-    run_data = run_local_stella_simulation('kxky_grid_singlemode_linear.in', tmp_path)
+    run_data = run_local_stella_simulation('kxky_grid_singlemode_linear.in', tmp_path, stella_version)
      
     # Compare phi2(t) in the netCDF files   
     compare_local_potential_with_expected_potential(run_data=run_data, error=False)    
